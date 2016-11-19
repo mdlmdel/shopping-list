e6c970e25e3891d7a35f3e60b2c70956c396647d
mdlmdel <mdlmdel@gmail.com>
2016-11-19 14:37:51 -0500

    Add if else statements for checked item and adapt render function and check item event listener

--- app.js
+++ app.js
@@ -6,91 +6,59 @@ var state = {
 // STATE MODIFICATION FUNCTIONS
 // Add item function
 var addItem = function(state, item) {
-    state.items.push(item);
-    console.log(state);
+    var newItem = {};
+    newItem.name = item;
+    newItem.checked = false;
+    state.items.push(newItem);
 };
-
-// Get items
-var getItem = function (state, itemIndex) {
-    state.itemIndex.return(item);
-}
-
-// RENDER FUNCTIONS
-var renderList = function(state, element) {
-    // Below is the new variable, "itemsHTML"
-    var itemsHTML = state.items.map(function(item, index) {
-        return '<li data-index="'+ index +'">' + '<span class="shopping-item">' + item + '</span>' + 
-        '<button class="shopping-item-toggle">' + '<span class="button-label">check</span>' + 
-        '</button>' + '&nbsp;' + '<button class="shopping-item-delete">' + 
-        '<span class="button-label">delete</span>' + '</button>' + '</li>';
-    })
-    element.html(itemsHTML);
-}
 
 // Remove item function
 var deleteItem = function(state, item) {
-    state.items.splice(item);
-    console.log(state);
-}
-
-// Update item function
-var updateItem = function (state, item) {
-    state.items.update(item);
-    console.log(state);
-}
-
-// Delete item function
-var deleteItem = function (state, item) {
     state.items.splice(item, 1);
-    console.log(state);
 }
 
 // Check item function
-var checkItem = function (state, item) {
-    $( ".shopping-item-toggle" ).addClass( "shopping-item-checked" );
-    console.log(state);
+var checkItem = function(state, item) {
+    state.items[item].checked = !state.items[item].checked;
 }
 
-// Get the index number of the item to be deleted
-var itemsHTML = state.items.map(function(item, index) {
-        return '<li data-index=' + index +'"">' + '<span class="shopping-item">' + item + '</span>' + 
-            '<button class="shopping-item-toggle">' + '<span class="button-label">check</span>' + 
+// RENDER FUNCTION
+var renderList = function(state, element) {
+    var itemsHTML = state.items.map(function(item, index) {
+        var html = "";
+        html += '<li data-index="' + index +'">'
+        if (item.checked) {
+            html += '<span class="shopping-item shopping-item-checked">'; 
+        } else {
+            html += '<span class="shopping-item">'; 
+        }
+        html += item.name + '</span><button class="shopping-item-toggle">' + '<span class="button-label">check</span>' + 
             '</button>' + '&nbsp;' + '<button class="shopping-item-delete">' + 
             '<span class="button-label">delete</span>' + '</button>' + '</li>';
+        return html;    
     });
+    element.html(itemsHTML);
+};
 
 // EVENT LISTENERS
 // Add item listener
 $('#js-shopping-list-form').submit(function(event) {
     event.preventDefault();
-    addItem(state, $('#shopping-list-entry').val()); // Pass this to the addItem... 
-    // ...with the state
-    // Clear form after each entry
+    addItem(state, $('#shopping-list-entry').val());
     $('#shopping-list-entry').val('');
     renderList(state, $('.shopping-list')); 
 });
 
-// Delete listener
-$(document).on('click', ".shopping-item-delete", function () {
-// Get the item, define the item
-// The attr of the parent is 'data-index'
+// Delete item listener
+$(document).on('click', '.shopping-item-delete', function() { 
     var item = parseInt($(this).parent().attr('data-index'));
-// Delete the item you've identified
     deleteItem(state, item);
-// Re-render the list now that you've deleted an item
     renderList(state, $('.shopping-list')); 
 });
 
-// Shopping item toggle
-// Add a class of shopping-item-checked and then render the list
-$(document).on('click',".shopping-item-toggle", function () {
-// Get the item, define the item
-// The attr of the parent is 'data-index'
+// Check item listener
+$(document).on('click', '.shopping-item-toggle', function() { 
     var item = parseInt($(this).parent().attr('data-index'));
-// Strike through the item you've identified
     checkItem(state, item);
-//      $(item, state).addClass("shopping-item-checked");
-//      $("#shopping-list-entry").wrap("<strike>");
-// Re-render the list now that you've checked an item
     renderList(state, $('.shopping-list')); 
 });
